0602db64960ec0b0b22961cb641dce31b9c2e125
Leonardo Gomide <leocgomide@gmail.com>
2019-05-26 18:50:22 -0300

    Initial Commit

---
+++ T2_LeonardoGomide_MariaEduardaPasquel_CalculadoraDeMateriaisDeRedeEstruturada.py
@@ -0,0 +1,12 @@
+
+
+
+
+pass
+print("Digite o numero de pontos de telecom:")
+while True:
+    try:
+        ptTelecom = int(input())
+        break
+    except ValueError:
+        print("Digite um numero inteiro:")
